454fcda0263a92abbdbcd58b95d0aa15491b4513
berkeuysal <berke_uysal_99@hotmail.com>
2021-10-19 00:13:06 +0300

    Değişiklikler

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhabalar AQ");
+
+let dolarbugun = 9.33;
+
+let dolardun = 9.2;
+
+{
+  let dolardun = 9.1;
+}
+
+console.log(dolardun);
+
+const eurodun = 11.2;
+//eurodun = 11;
+
+console.log(eurodun);
+
+//array
+//camel casing
+// pascal casing
+let konutkredileri = [
+  "Konut Kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+  "Özel Konut Kredisi",
+];
+
+console.log("<ul>");
+for (let index = 0; index < konutkredileri.length; index++) {
+  console.log("<li>" + konutkredileri[index] + "</li>");
+}
+console.log(konutkredileri);
